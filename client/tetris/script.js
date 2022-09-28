7f4c2b1102070cdaf4fd5a808e8444c3d7e139ff
William Jones <wjones19@umd.edu>
2022-09-28 08:34:23 -0400

    JS file updates

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -1,3 +1,46 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('grid');
+  let squares = Array.from(document.querySelectorAll('.grid div'));
+  const width = 10;
+  const ScoreDisplay = document.querySelector('#score');
+  const StartBnt = document.querySelector('#start-button');
 
-})
+  //   Tetrominoes
+  const lTetromino = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2],
+  ];
+
+  const zTetromino = [
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1],
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1]
+  ]
+
+  const tTetromino = [
+    [1,width,width+1,width+2],
+    [1,width+1,width+2,width*2+1],
+    [width,width+1,width+2,width*2+1],
+    [1,width,width+1,width*2+1]
+  ]
+
+  const oTetromino = [
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1]
+  ]
+
+  const iTetromino = [
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3],
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3]
+  ]
+
+
+  
+});
