790df5082f2becc6dee9e6e6aadd52e0db5f661c
William Jones <wjones19@umd.edu>
2022-09-28 11:50:33 -0400

    Move left/right working

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -86,4 +86,47 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // make the tetromino move down every second
   timerId = setInterval(moveDown, 1000);
+
+  // assign functions to keyCodes
+  function control(e) {
+    if (e.keyCode === 37) {
+      moveLeft();
+    } else if (e.keyCode === 38) {
+      // rotate
+
+    } else if (e.keyCode === 39) {
+      moveRight();
+    } else if (e.keyCode === 40) {
+      moveDown();
+    }
+  }
+  document.addEventListener('keyup', control);
+
+  // move the tetromino left
+  function moveLeft() {
+    undraw();
+    const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0);
+    if (!isAtLeftEdge) currentPosition -= 1;
+    if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition += 1;
+    }
+    draw();
+  }
+
+  // move the tetromino right
+  function moveRight() {
+    undraw();
+    const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1);
+    if (!isAtRightEdge) currentPosition += 1;
+    if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition -= 1;
+    }
+    draw();
+  }
+  
+  // rotating the Tetromino
+  function rotate() {
+    undraw();
+    currentRotation ++ 
+  }
 });
