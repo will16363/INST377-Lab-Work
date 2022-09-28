0d30fff27dc287e53351d31dedbb8a82a9b68997
William Jones <wjones19@umd.edu>
2022-09-28 09:53:52 -0400

    Changes

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const ScoreDisplay = document.querySelector('#score');
   const StartBnt = document.querySelector('#start-button');
 
-  //   Tetrominoes
+  //Tetrominoes
   const lTetromino = [
     [1, width + 1, width * 2 + 1, 2],
     [width, width + 1, width + 2, width * 2 + 2],
@@ -18,29 +18,42 @@ document.addEventListener('DOMContentLoaded', () => {
     [width+1, width+2,width*2,width*2+1],
     [0,width,width+1,width*2+1],
     [width+1, width+2,width*2,width*2+1]
-  ]
+  ];
 
   const tTetromino = [
     [1,width,width+1,width+2],
     [1,width+1,width+2,width*2+1],
     [width,width+1,width+2,width*2+1],
     [1,width,width+1,width*2+1]
-  ]
+  ];
 
   const oTetromino = [
     [0,1,width,width+1],
     [0,1,width,width+1],
     [0,1,width,width+1],
     [0,1,width,width+1]
-  ]
+  ];
 
   const iTetromino = [
     [1,width+1,width*2+1,width*3+1],
     [width,width+1,width+2,width+3],
     [1,width+1,width*2+1,width*3+1],
     [width,width+1,width+2,width+3]
-  ]
+  ];
+
+  const theTetrominoes = [
+    lTetromino, zTetromino, tTetromino, oTetromino, iTetromino
+  ];
+
+  let currentPosition = 4;
+  let current = theTetrominoes[0];
+
+  //drawing first rotation in first tetromino
+  function draw() {
+    current.forEach(index => {
+        squares[currentPosition + index].classList.add('tetromino');
+    })
 
 
-  
+
 });
