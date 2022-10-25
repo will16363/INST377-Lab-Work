9fe60aee5ba8695e9684055c377ba1b021c695ae
wjones19@umd.edu <79867630+will16363@users.noreply.github.com>
2022-10-25 12:15:03 -0400

    Almost done

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -33,6 +33,15 @@ function getRandomIntInclusive(min, max) {
 	return newArray;
   }
   
+  function filterList(list, filterInputValue) {
+	return list.filter((item) => {
+	  if (!item.name) { return; }
+		const lowerCaseName = item.name.toLowerCase();
+		const lowerCaseQuery = filterInputValue.toLowerCase();
+		return lowerCaseName.includes(lowerCaseQuery);
+    });
+  }
+
   async function mainEvent() {
 	/*
 	  ## Main Event
@@ -76,7 +85,15 @@ function getRandomIntInclusive(min, max) {
   
 	  loadAnimation.classList.remove('lds-ellipsis');
 	  loadAnimation.classList.add('lds-ellipsis_hidden');
+
+	  let currentList = [];
   
+	  form.addEventListener('input', (event)=> {
+		console.log(event.target.value);
+		const filteredtList = filterList(currentList, event.target.value);
+		injectHTML(filteredList);
+	  });
+
 	  // And here's an eventListener! It's listening for a "submit" button specifically being clicked
 	  // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
 	  form.addEventListener('submit', (submitEvent) => {
@@ -84,11 +101,10 @@ function getRandomIntInclusive(min, max) {
 		submitEvent.preventDefault();
   
 		// This constant will have the value of your 15-restaurant collection when it processes
-		const restaurantList = processRestaurants(arrayFromJson.data);
-		console.log(restaurantList);
+		currentList = processRestaurants(arrayFromJson.data);
   
 		// And this function call will perform the "side effect" of injecting the HTML list for you
-		injectHTML(restaurantList);
+		injectHTML(currentList);
   
 		// By separating the functions, we open the possibility of regenerating the list
 		// without having to retrieve fresh data every time
